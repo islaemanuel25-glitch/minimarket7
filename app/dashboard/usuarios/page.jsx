5f7b79434d79b8111853da1d50f5d1a2084b3de9
islaemanuel25-glitch <islaemanuel25@gmail.com>
2025-10-11 12:10:59 -0300

    Create page.jsx

---
+++ app/dashboard/usuarios/page.jsx
@@ -0,0 +1,8 @@
+export default function Usuarios() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Configuración → Usuarios</h1>
+      <p>Desde acá vas a poder crear, editar o eliminar usuarios del sistema.</p>
+    </div>
+  );
+}
