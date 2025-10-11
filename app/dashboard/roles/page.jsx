e2e5c7606fa7da86bdddf470913067252030e5c4
islaemanuel25-glitch <islaemanuel25@gmail.com>
2025-10-11 12:04:21 -0300

    Create page.jsx

---
+++ app/dashboard/roles/page.jsx
@@ -0,0 +1,8 @@
+export default function Roles() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Configuración → Roles</h1>
+      <p>Aquí vas a poder gestionar los diferentes tipos de usuarios y permisos.</p>
+    </div>
+  );
+}
