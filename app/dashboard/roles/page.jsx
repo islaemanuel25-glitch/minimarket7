a2813e29bb607a03f41a45206e1a7568b41ddec2
islaemanuel25-glitch <islaemanuel25@gmail.com>
2025-10-11 14:02:07 -0300

    Delete app/dashboard/roles/page.jsx

--- app/dashboard/roles/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Roles() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Configuración → Roles</h1>
-      <p>Aquí vas a poder gestionar los diferentes tipos de usuarios y permisos.</p>
-    </div>
-  );
-}
